fa011dcded68dbe4b2c674a67fd389870dd2fd6a
BARRACK123 <jdaizybarrack@gmail.com>
2023-06-15 09:59:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+INT MAIN (void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0)
+}	
