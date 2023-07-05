5b866ecf662eb341e21ce81b859708be30691f78
BARRACK123 <jdaizybarrack@gmail.com>
2023-07-05 08:01:57 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,17 @@
+#include"mainh"
+**
+*_strlen-Return the length of  a string
+*@s: The string to be measured.
+* Return: The length of the string.
+*/
+int_strlenn_recursion(char*s)
+{
+	int longit=0;
+	if (s) 
+	{
+		longit++
+			longit+=_strlen_ recursion(s +1) 
 
+	} 
+	return (longit);
+}
